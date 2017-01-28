41b2e74609282b0476646ef6365364d6dbaede84
conscientiousCode <conscientiouscode@outlook.com>
2017-01-27 17:18:31 -0800

    Created interfaces and base data type classes

---
+++ Research Source Manager/src/sourcedatatypes/Title.java
@@ -0,0 +1,5 @@
+package sourcedatatypes;
+
+public class Title {
+
+}
---
+++ Research Source Manager/src/sourceformats/Source.java
@@ -0,0 +1,12 @@
+package sourceformats;
+import sourceformats;
+
+public interface Source{
+
+	//Returns an array of Strings representing Authors
+	Author[] getAuthors();
+	
+	
+	
+	
+}
